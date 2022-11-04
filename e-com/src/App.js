dba34d8810feba6597b298e606cd9a7d458870dc
Samrat <samratkrishna345@gmail.com>
2022-11-04 20:28:08 +0530

    Routes added

--- e-com/src/App.js
+++ e-com/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import SingleProduct from './pages/SingleProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path ="/about" element = {<About />} />
+          <Route path ="/contact" element = {<Contact />} />
+          <Route path ="/products" element = {<Products />} />
+          <Route path ="/singleproduct/:id" element = {<SingleProduct />} />
+          <Route path = "/cart" element ={<Cart />} />
+          <Route path = "error" element ={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
